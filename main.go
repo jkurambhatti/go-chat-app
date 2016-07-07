a009b840baf7cc8a6decd2152133d55d4b02e829
jkurambhatti <jkurambhatti2992@gmail.com>
2016-07-07 11:04:05 +0530

    added html templating

--- main.go
+++ main.go
@@ -2,11 +2,26 @@ package main
 
 import (
 	"fmt"
+	"html/template"
 	"log"
 	"net/http"
 )
 
+type User struct {
+	Name string `json:"name"`
+}
+
 func index(w http.ResponseWriter, req *http.Request) {
+	tmp, err := template.ParseFiles("index.html")
+	if err != nil {
+		fmt.Errorf("template.ParseFiles %s \n", err)
+	}
+	u := User{Name: "Jayesh"}
+	if err = tmp.Execute(w, u); err != nil {
+		log.Println("tmp.Execute: ", err)
+		return
+	}
+
 	w.Write([]byte("a simple chat application"))
 }
 
